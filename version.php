0cf64fd8cc16faf3b09f5a41f7cb79846e7a6fa5
Gareth J Barnard <gjbarnard@gmail.com>
2013-01-21 21:09:48 +0000

    V2.4.2

--- version.php
+++ version.php
@@ -33,6 +33,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2013011700;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->requires = 2012120300.00; // 2.4 (Build: 20121203)
+$plugin->requires = 2012120301.02; // 2.4.1+ (Build: 20130118)
 $plugin->component = 'format_topcoll';
 $plugin->release = '2.4.2';
