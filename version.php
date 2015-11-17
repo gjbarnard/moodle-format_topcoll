3cd8a1d0641419158d76a11a6725329d7e1a6b02
Gareth J Barnard <gjbarnard@gmail.com>
2015-11-17 12:26:42 +0000

    V2.7.4

--- version.php
+++ version.php
@@ -38,4 +38,4 @@ $plugin->version = 2014092005;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2014051200.00; // 2.7 (Build: 20140512).
 $plugin->component = 'format_topcoll';
-$plugin->release = '2.7.3.3';
+$plugin->release = '2.7.4';
