886543cb6e620f93db8a163515d11524d28994c0
Gareth J Barnard <gjbarnard@gmail.com>
2015-12-12 12:24:27 +0000

    V2.7.4.1

--- version.php
+++ version.php
@@ -34,8 +34,8 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2014092005;
+$plugin->version = 2014092006;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2014051200.00; // 2.7 (Build: 20140512).
 $plugin->component = 'format_topcoll';
-$plugin->release = '2.7.4';
+$plugin->release = '2.7.4.1';
