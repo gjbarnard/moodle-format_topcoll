c1d4d4c0198da92ea67aa4130fd727d70191d03d
Gareth J Barnard <gjbarnard@gmail.com>
2018-06-14 17:20:37 +0100

    Travis CI issues.

--- tests/privacy_provider_test.php
+++ tests/privacy_provider_test.php
@@ -131,17 +131,18 @@ class format_topcoll_privacy_testcase extends \core_privacy\tests\provider_testc
 
         $this->assertTrue($writer->has_any_data());
 
-        $prefs = $writer->get_user_preferences('format_topcoll');
+        $prefs = (array) $writer->get_user_preferences('format_topcoll');
 
-        $this->assertCount(1, (array) $prefs);
+        $this->assertCount(1, $prefs);
 
-        $this->assertEquals('FAB', ((array)$prefs)['topcoll_toggle_'.$this->course->id]->value);
+        $toggle = $prefs['topcoll_toggle_'.$this->course->id];
+        $this->assertEquals('FAB', $toggle->value);
 
         $description = get_string('privacy:request:preference:toggle', 'format_topcoll', (object) [
             'name' => $this->course->id,
             'value' => 'FAB',
             'decoded' => $togglelib->decode_toggle_state('FAB'),
         ]);
-        $this->assertEquals($description, ((array)$prefs)['topcoll_toggle_'.$this->course->id]->description);
+        $this->assertEquals($description, $toggle->description);
     }
 }
