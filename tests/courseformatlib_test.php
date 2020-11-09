f829c1b82cd82181ecdd9206554879c67f5d24fd
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2020-11-09 22:46:02 +0000

    PHPUnit update on Travis.

--- tests/courseformatlib_test.php
+++ tests/courseformatlib_test.php
@@ -35,7 +35,7 @@ class format_topcoll_courseformatlib_testcase extends advanced_testcase {
     protected $course;
     protected $courseformat;
 
-    protected function setUp() {
+    protected function setUp(): void {
         $this->resetAfterTest(true);
 
         set_config('theme', 'boost');
