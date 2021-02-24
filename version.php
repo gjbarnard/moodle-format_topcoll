706fac1f3362b3e423b6063b2a74e81a7ca80eb8
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2021-02-24 17:55:53 +0000

    V3.10.1.0

--- version.php
+++ version.php
@@ -33,8 +33,8 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020110903;
-$plugin->maturity = MATURITY_BETA;
+$plugin->version = 2020110904;
+$plugin->maturity = MATURITY_STABLE;
 $plugin->requires  = 2020110900.00; // 3.10 (Build: 20201109).
 $plugin->component = 'format_topcoll';
-$plugin->release = '3.10.0.3';
+$plugin->release = '3.10.1.0';
