94f3974a01df1d9b0edd70b06d990bc2e1f8f579
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2021-06-25 23:48:57 +0100

    Supported Moodle version.

--- version.php
+++ version.php
@@ -36,5 +36,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2021061200;
 $plugin->maturity = MATURITY_RC;
 $plugin->requires  = 2021051700.00; // 3.11 (Build: 20210517).
+$plugin->supported = array(311, 311);
 $plugin->component = 'format_topcoll';
 $plugin->release = '3.11.0.1';
