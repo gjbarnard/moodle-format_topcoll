8b1480a904f4e4e5911e4ba37893211f6e29154c
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2021-10-22 16:37:36 +0100

    V4.0.0.1 work in progress.

---
+++ classes/output/courseformat/content/section/controlmenu.php
@@ -0,0 +1,184 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Contains the default section controls output class.
+ *
+ * @package   format_topics
+ * @copyright 2020 Ferran Recio <ferran@moodle.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace format_topcoll\output\courseformat\content\section;
+
+use core_courseformat\output\local\content\section\controlmenu as controlmenu_base;
+use context_course;
+use moodle_url;
+use pix_icon;
+use action_menu_link_secondary;
+use action_menu;
+use stdClass;
+
+/**
+ * Base class to render a course section menu.
+ *
+ * @package   format_topics
+ * @copyright 2020 Ferran Recio <ferran@moodle.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class controlmenu extends controlmenu_base {
+
+    /** @var course_format the course format class */
+    protected $format;
+
+    /** @var section_info the course section class */
+    protected $section;
+
+    /**
+     * Export this data so it can be used as the context for a mustache template.
+     *
+     * @param renderer_base $output typically, the renderer that's calling this function
+     * @return array data context for a mustache template
+     */
+    public function export_for_template(\renderer_base $output): stdClass {
+        $section = $this->section;
+
+        $controls = $this->section_control_items();
+
+        if (empty($controls)) {
+            return new stdClass();
+        }
+
+        // Convert control array into an action_menu.
+        $menu = new action_menu();
+        $menu->set_menu_trigger(get_string('edit'));
+        $menu->attributes['class'] .= ' section-actions';
+        foreach ($controls as $value) {
+            $url = empty($value['url']) ? '' : $value['url'];
+            $icon = empty($value['icon']) ? '' : $value['icon'];
+            $name = empty($value['name']) ? '' : $value['name'];
+            $attr = empty($value['attr']) ? [] : $value['attr'];
+            $class = empty($value['pixattr']['class']) ? '' : $value['pixattr']['class'];
+            $al = new action_menu_link_secondary(
+                new moodle_url($url),
+                new pix_icon($icon, '', null, ['class' => "smallicon " . $class]),
+                $name,
+                $attr
+            );
+            $menu->add($al);
+        }
+
+        $course = $this->format->get_course();
+        $coursecontext = context_course::instance($course->id);
+        if (has_capability('moodle/course:manageactivities', $coursecontext)) {
+            $duplicatestr = get_string('duplicate', 'format_topcoll');
+            $duplicateurl = new moodle_url('/course/format/topcoll/duplicate.php',
+                array('courseid' => $course->id, 'sectionno' => $section->section, 'sesskey' => sesskey()));
+            $link = new \action_link($duplicateurl, ' '.$duplicatestr, null,
+                array('class' => 'menu-action', 'role' => 'menuitem'),
+                new \pix_icon('t/copy', $duplicatestr));
+            $link->add_action(new \confirm_action(get_string('duplicateconfirm', 'format_topcoll'), null,
+                $duplicatestr));
+            $menu->add_secondary_action($link);
+        }
+
+        $data = (object)[
+            'menu' => $output->render($menu),
+            'hasmenu' => true,
+            'id' => $section->id,
+        ];
+
+        return $data;
+    }
+
+    /**
+     * Generate the edit control items of a section.
+     *
+     * This method must remain public until the final deprecation of section_edit_control_items.
+     *
+     * @return array of edit control items
+     */
+    public function section_control_items() {
+
+        $format = $this->format;
+        $section = $this->section;
+        $course = $format->get_course();
+        $sectionreturn = $format->get_section_number();
+
+        $coursecontext = context_course::instance($course->id);
+
+        if ($sectionreturn) {
+            $url = course_get_url($course, $section->section);
+        } else {
+            $url = course_get_url($course);
+        }
+        $url->param('sesskey', sesskey());
+
+        $tcsettings = $format->get_settings();
+
+        $controls = [];
+        if ((($tcsettings['layoutstructure'] == 1) || ($tcsettings['layoutstructure'] == 4)) &&
+            $section->section && has_capability('moodle/course:setcurrentsection', $coursecontext)) {
+            if ($course->marker == $section->section) {  // Show the "light globe" on/off.
+                $url->param('marker', 0);
+                $highlightoff = get_string('highlightoff');
+                $controls['highlight'] = [
+                    'url' => $url,
+                    'icon' => 'i/marked',
+                    'name' => $highlightoff,
+                    'pixattr' => ['class' => ''],
+                    'attr' => [
+                        'class' => 'editing_highlight',
+                        'data-action' => 'removemarker'
+                    ],
+                ];
+            } else {
+                $url->param('marker', $section->section);
+                $highlight = get_string('highlight');
+                $controls['highlight'] = [
+                    'url' => $url,
+                    'icon' => 'i/marker',
+                    'name' => $highlight,
+                    'pixattr' => ['class' => ''],
+                    'attr' => [
+                        'class' => 'editing_highlight',
+                        'data-action' => 'setmarker'
+                    ],
+                ];
+            }
+        }
+
+        $parentcontrols = parent::section_control_items();
+
+        // If the edit key exists, we are going to insert our controls after it.
+        if (array_key_exists("edit", $parentcontrols)) {
+            $merged = [];
+            // We can't use splice because we are using associative arrays.
+            // Step through the array and merge the arrays.
+            foreach ($parentcontrols as $key => $action) {
+                $merged[$key] = $action;
+                if ($key == "edit") {
+                    // If we have come to the edit key, merge these controls here.
+                    $merged = array_merge($merged, $controls);
+                }
+            }
+
+            return $merged;
+        } else {
+            return array_merge($controls, $parentcontrols);
+        }
+    }
+}
