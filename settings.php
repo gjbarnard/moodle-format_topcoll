b0d3ed3e814d93799cdc4e148a8b909b234f6cbe
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2023-05-10 19:14:17 +0100

    Forgot!

--- settings.php
+++ settings.php
@@ -44,7 +44,7 @@ if ($ADMIN->fulltree) {
         format_text(get_string('informationsettingsdesc', 'format_topcoll'), FORMAT_MARKDOWN)));
 
     // Information.
-    $page->add(new \format_topcoll\admin_setting_information('format_topcoll/formatinformation', '', '', 401));
+    $page->add(new \format_topcoll\admin_setting_information('format_topcoll/formatinformation', '', '', 402));
 
     // Support.md.
     $page->add(new \format_topcoll\admin_setting_markdown('format_topcoll/formatsupport', '', '', 'Support.md'));
