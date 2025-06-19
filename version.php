ee91dbe3cc128c44aef0a736f1261a4c5dfade8c
eranac <77002066+eranac@users.noreply.github.com>
2025-06-19 11:20:47 +0300

    Update version.php
    Signed-off-by: eranac <77002066+eranac@users.noreply.github.com>

--- version.php
+++ version.php
@@ -32,7 +32,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2025041700;
 $plugin->maturity = MATURITY_RC;
-$plugin->requires = 2025041400.00; // 5.0 (Build: 20250414).
+$plugin->requires = 2022112818; // 5.0 (Build: 20250414).
 $plugin->supported = [500, 500];
 $plugin->component = 'format_topcoll';
 $plugin->release = '500.0.1';
